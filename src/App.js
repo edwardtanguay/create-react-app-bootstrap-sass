6c90831c43f93fe7a58982ff0ab1ec9052e30cc1
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-13 01:33:59 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<div className="App container mt-5">
+			<h1 className="text-primary mb-4"><i className="bi bi-diagram-2-fill"></i> React-Bootstrap-Sass Site</h1>
+			<p>Welcome to this site.</p>
+		</div>
+	);
+}
+
+export default App;
